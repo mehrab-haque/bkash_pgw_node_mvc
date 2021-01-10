dafc6999912130325626ea7c4cd7935348ab0170
mehrab-haque <mehrab.haque.0001@gmail.com>
2021-01-10 16:26:30 +0600

    dummy commit

--- service/onBoardingLogger.js
+++ service/onBoardingLogger.js
@@ -3,7 +3,7 @@ const fs=require('fs')
 module.exports={
     log:async (type,url,request,header,response)=>{
         return
-        //var string='url : '+url+'\n\nrequest body : \n'+JSON.stringify(request)+'\n\n request headers : \n'+JSON.stringify(header)+'\n\nresponse body : \n'+JSON.stringify(response)
+        var string='url : '+url+'\n\nrequest body : \n'+JSON.stringify(request)+'\n\n request headers : \n'+JSON.stringify(header)+'\n\nresponse body : \n'+JSON.stringify(response)
         //fs.writeFileSync('log/'+type+'_'+Date.now(),string)
     }
 }
