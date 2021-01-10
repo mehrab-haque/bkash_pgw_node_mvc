47109366ae8910db23df176fef6a135a97411889
mehrab-haque <mehrab.haque.0001@gmail.com>
2021-01-10 16:22:54 +0600

    production beta commit

--- service/onBoardingLogger.js
+++ service/onBoardingLogger.js
@@ -2,7 +2,8 @@ const fs=require('fs')
 
 module.exports={
     log:async (type,url,request,header,response)=>{
-        var string='url : '+url+'\n\nrequest body : \n'+JSON.stringify(request)+'\n\n request headers : \n'+JSON.stringify(header)+'\n\nresponse body : \n'+JSON.stringify(response)
-        fs.writeFileSync('log/'+type+'_'+Date.now(),string)
+        return
+        //var string='url : '+url+'\n\nrequest body : \n'+JSON.stringify(request)+'\n\n request headers : \n'+JSON.stringify(header)+'\n\nresponse body : \n'+JSON.stringify(response)
+        //fs.writeFileSync('log/'+type+'_'+Date.now(),string)
     }
 }
